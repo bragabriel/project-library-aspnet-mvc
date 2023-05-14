953ba3dbdd50c66bf40da9a7e8ae8e5d750bf8b8
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2023-05-14 15:25:51 -0300

    initial commit; model and base repository

---
+++ LivrariaControleEmprestimo.DATA/Repositories/RepositoryBase.cs
@@ -0,0 +1,80 @@
+﻿using LivrariaControleEmprestimo.DATA.Interfaces;
+using LivrariaControleEmprestimo.DATA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivrariaControleEmprestimo.DATA.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryModel<T>, IDisposable where T : class
+    {
+        protected ControleEmprestimoLivroContext _Contexto;
+        public bool _SaveChanges = true;
+
+        public RepositoryBase(bool saveChanges = true)
+        {
+            _SaveChanges = saveChanges;
+            _Contexto = new ControleEmprestimoLivroContext();
+        }
+
+        public T Alterar(T objeto)
+        {
+            _Contexto.Entry(objeto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+          
+            if (_SaveChanges)
+            {
+                _Contexto.SaveChanges();
+            }
+            return objeto;
+        }
+
+        public void Dispose()
+        {
+            _Contexto.Dispose();
+        }
+
+        public void Excluir(T objeto)
+        {
+            _Contexto.Set<T>().Remove(objeto);
+
+            if (_SaveChanges)
+            {
+                _Contexto.SaveChanges();
+            }
+        }
+
+        public void Excluir(params object[] variavel)
+        {
+            var objeto = SelecionarPk(variavel);
+            Excluir(objeto);
+        }
+
+        public T Incluir(T objeto)
+        {
+            _Contexto.Set<T>().Add(objeto);
+            
+            if (_SaveChanges)
+            {
+                _Contexto.SaveChanges();
+            }
+            return objeto;
+        }
+
+        public void SaveChanges()
+        {
+            _Contexto.SaveChanges();
+        }
+
+        public T SelecionarPk(params object[] variavel)
+        {
+            return _Contexto.Set<T>().Find(variavel);
+        }
+
+        public List<T> SelecionarTodos()
+        {
+            return _Contexto.Set<T>().ToList();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# project-library-aspnet-mvc
+Projeto em **APS.NET Core** utilizando arquitetura **MVC**
+
+### DataBase
+* SQL Server.
+
+### Project Dependencies
+* entityframework core;
+* entityframework sql;
+* entityframework design.
