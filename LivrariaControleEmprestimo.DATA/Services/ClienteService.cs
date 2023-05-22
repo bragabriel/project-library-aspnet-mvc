f55b6d5cbf3dac3bdb1b13662b5602e0113c5690
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2023-05-22 14:08:35 -0300

    feat: Controller, Service and Views - Livro

--- LivrariaControleEmprestimo.DATA/Services/ClienteService.cs
+++ LivrariaControleEmprestimo.DATA/Services/ClienteService.cs
@@ -1,9 +1,4 @@
 ﻿using LivrariaControleEmprestimo.DATA.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LivrariaControleEmprestimo.DATA.Services
 {
---
+++ LivrariaControleEmprestimo.WEB/Controllers/LivroController.cs
@@ -0,0 +1,63 @@
+﻿using LivrariaControleEmprestimo.DATA.Models;
+using LivrariaControleEmprestimo.DATA.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LivrariaControleEmprestimo.WEB.Controllers
+{
+    public class LivroController : Controller
+    {
+        private LivroService oLivroService = new LivroService();
+
+        public IActionResult Index()
+        {
+            List<Livro> oListLivro = oLivroService.oRepositoryLivro.SelecionarTodos();
+            return View(oListLivro);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Livro model)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            oLivroService.oRepositoryLivro.Incluir(model);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            Livro oLivro = oLivroService.oRepositoryLivro.SelecionarPk(id);
+
+            return View(oLivro);
+        }
+        public IActionResult Edit(int id)
+        {
+            Livro oLivro = oLivroService.oRepositoryLivro.SelecionarPk(id);
+
+            return View(oLivro);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Livro model)
+        {
+            Livro oLivro = oLivroService.oRepositoryLivro.Alterar(model);
+
+            int id = oLivro.Id;
+
+            return RedirectToAction("Details", new { id });
+        }
+
+        public IActionResult Delete(int id)
+        {
+            oLivroService.oRepositoryLivro.Excluir(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
